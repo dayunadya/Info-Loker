5c07d6307e7166e031a519a67f7696bc6d6e96e1
dayunadya <nadyamaharani01.nmd@gmail.com>
2024-01-11 14:38:02 +0800

    design company page

--- app/view/perusahaan/loker.php
+++ app/view/perusahaan/loker.php
@@ -6,8 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/styleprs.css">
-    <link rel="stylesheet" href="http://localhost/JobVacancy/public/css/style.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="<?= BASEURL ?>/js/lokerjs.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-rqO56EATt7i0z6flqQzRII5k9EodVrZJu7WrWl+UnZ9AtX2f8b3LX6d65DgVPdN" crossorigin="anonymous"></script>
@@ -19,12 +22,13 @@
     <!-- <script src="<?= BASEURL ?>/js/script.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    <nav class="navbar navbar-expand-lg navy-dsbrd1">
+
+    <nav class="navbar navbar-expand-lg navy-dsbrd1 position-sticky">
         <div class="container-fluid">
             <span class="navbar-brand fw-bold text-light navy-dsbrd2">Management Loker</span>
             <div class="d-flex align-items-center">
                 <div class="d-flex align-items-center">
-                    <a href="<?= BASEURL; ?>/welcome/prosesLogout"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+                    <a href="<?= BASEURL; ?>/welcome/prosesLogout"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;">
                             <path d="m2 12 5 4v-3h9v-2H7V8z"></path>
                             <path d="M13.001 2.999a8.938 8.938 0 0 0-6.364 2.637L8.051 7.05c1.322-1.322 3.08-2.051 4.95-2.051s3.628.729 4.95 2.051 2.051 3.08 2.051 4.95-.729 3.628-2.051 4.95-3.08 2.051-4.95 2.051-3.628-.729-4.95-2.051l-1.414 1.414c1.699 1.7 3.959 2.637 6.364 2.637s4.665-.937 6.364-2.637c1.7-1.699 2.637-3.959 2.637-6.364s-.937-4.665-2.637-6.364a8.938 8.938 0 0 0-6.364-2.637z"></path>
                         </svg></a>
@@ -41,12 +45,14 @@
                         <img src="<?= BASEURL;?>/img/logo.png" alt="Logo" class="img-fluid">
                     </div>
                     <ul class="sidebar-menu mt-5 sidebar-dsbrd2">
-                        <li><i class='bx bxs-home text-light'></i>
-                            <a href="<?= BASEURL; ?>/perusahaan">Beranda</a>
+                        <li>
+                            <i class="fa-solid fa-house me-2" style="color: white;"></i>
+                            <a href="<?= BASEURL; ?>/perusahaan" class="span" style="text-decoration: none; color: white;">Beranda</a>
                         </li>
                         <br></br>
-                        <li><i class='bx bxs-home text-light'></i>
-                            <a href="<?= BASEURL; ?>/perusahaan/loker">Loker</a>
+                        <li>
+                            <i class="fa-solid fa-book me-2" style="color: white;"></i>
+                            <a href="<?= BASEURL; ?>/perusahaan/loker" class="span" style="text-decoration: none; color: white;">Loker</a>
                         </li>
                         <br></br>
                         <!-- <li><i class='bx bx-buildings text-light'></i>
@@ -64,6 +70,8 @@
 
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                <h2 class="text-center p-3">Data Loker</h2>
                 <!-- new end -->
                 <div class="container mt-3">
                     <div class="row">
@@ -77,13 +85,13 @@
 
                             <!-- Button trigger modal -->
                             
-                            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: 0;msFilter:0;">
+                            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: 0;msFilter:0;">
                                     <path d="M4.5 8.552c0 1.995 1.505 3.5 3.5 3.5s3.5-1.505 3.5-3.5-1.505-3.5-3.5-3.5-3.5 1.505-3.5 3.5zM19 8h-2v3h-3v2h3v3h2v-3h3v-2h-3zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2z"></path>
                                 </svg>
                                 Tambah Loker
                             </button>
                             <!-- new -->
-                            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                            <main class="table table-bordered mt-4">
                                 <table class="table table-hover">
 
                                     <tr>
